44a486a16bffa59691165908d0de1eb8a24a8f36
Yahya Allam <yallam122@gmail.com>
2021-02-09 04:03:44 +0200

    Create user model

--- ClientSide/src/main/java/jets/chatclient/gui/models/CurrentUserModel.java
+++ ClientSide/src/main/java/jets/chatclient/gui/models/CurrentUserModel.java
@@ -1,24 +1,36 @@
 package jets.chatclient.gui.models;
 
+import java.time.LocalDate;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class CurrentUserModel {
 
-    private final StringProperty username = new SimpleStringProperty();
-    private final StringProperty password = new SimpleStringProperty();
-    private final StringProperty email = new SimpleStringProperty();
+    private StringProperty userName = new SimpleStringProperty();
+    private StringProperty password = new SimpleStringProperty();
+    private StringProperty phoneNumber = new SimpleStringProperty();
+    private StringProperty displayName = new SimpleStringProperty();
+    private StringProperty emailAddress = new SimpleStringProperty();
+    private StringProperty photoPath = new SimpleStringProperty();
+    private StringProperty gender = new SimpleStringProperty();
+    private StringProperty country = new SimpleStringProperty();
+    private ObjectProperty<LocalDate> birthdayDate = new SimpleObjectProperty<LocalDate>();
+    private StringProperty bio = new SimpleStringProperty();
 
-    public String getUsername() {
-        return username.get();
+
+    public String getUserName() {
+        return userName.get();
     }
 
-    public StringProperty usernameProperty() {
-        return username;
+    public StringProperty userNameProperty() {
+        return userName;
     }
 
-    public void setUsername(String username) {
-        this.username.set(username);
+    public void setUserName(String userName) {
+        this.userName.set(userName);
     }
 
     public String getPassword() {
@@ -33,15 +45,100 @@ public class CurrentUserModel {
         this.password.set(password);
     }
 
-    public String getEmail() {
-        return email.get();
+    public String getPhoneNumber() {
+        return phoneNumber.get();
     }
 
-    public StringProperty emailProperty() {
-        return email;
+    public StringProperty phoneNumberProperty() {
+        return phoneNumber;
     }
 
-    public void setEmail(String email) {
-        this.email.set(email);
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber.set(phoneNumber);
     }
+
+    public String getDisplayName() {
+        return displayName.get();
+    }
+
+    public StringProperty displayNameProperty() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName.set(displayName);
+    }
+
+    public String getEmailAddress() {
+        return emailAddress.get();
+    }
+
+    public StringProperty emailAddressProperty() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress.set(emailAddress);
+    }
+
+    public String getPhotoPath() {
+        return photoPath.get();
+    }
+
+    public StringProperty photoPathProperty() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath.set(photoPath);
+    }
+
+    public String getGender() {
+        return gender.get();
+    }
+
+    public StringProperty genderProperty() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender.set(gender);
+    }
+
+    public String getCountry() {
+        return country.get();
+    }
+
+    public StringProperty countryProperty() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country.set(country);
+    }
+
+    public LocalDate getBirthdayDate() {
+        return birthdayDate.get();
+    }
+
+    public ObjectProperty<LocalDate> birthdayDateProperty() {
+        return birthdayDate;
+    }
+
+    public void setBirthdayDate(LocalDate birthdayDate) {
+        this.birthdayDate.set(birthdayDate);
+    }
+
+	public String getBio() {
+        return bio.get();
+    }
+
+    public StringProperty bioProperty() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio.set(bio);
+    }
+
 }
